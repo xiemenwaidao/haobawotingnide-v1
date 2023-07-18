c38f31f9e0e878a5841fddd4469aee60322f63d5
xiemenwaidao <menxielijiao@gmail.com>
2023-07-18 16:09:51 +0900

    🔥 src/config.ts

---
+++ src/config.ts
@@ -0,0 +1,141 @@
+import type { Site, SocialObjects } from "./types/common";
+
+export const SITE: Site = {
+    website: "https://www.haobawotingnide.xyz/",
+    author: "Menxie Lijiao",
+    desc: "日々の開発記録 - フロントエンドや3Dの話題を共有するブログ。開発情報やノウハウ、新しい技術について発信しています。",
+    title: "HAOBAWOTINGNIDE",
+    ogImage: "ogImage.png",
+    lightAndDarkMode: true,
+    postPerPage: 3,
+};
+
+export const LOGO_IMAGE = {
+    enable: false,
+    svg: true,
+    width: 216,
+    height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+    {
+        name: "Github",
+        href: "https://github.com/satnaing/astro-paper",
+        linkTitle: ` ${SITE.title} on Github`,
+        active: false,
+    },
+    {
+        name: "Facebook",
+        href: "https://github.com/satnaing/astro-paper",
+        linkTitle: `${SITE.title} on Facebook`,
+        active: false,
+    },
+    {
+        name: "Instagram",
+        href: "https://github.com/satnaing/astro-paper",
+        linkTitle: `${SITE.title} on Instagram`,
+        active: false,
+    },
+    {
+        name: "LinkedIn",
+        href: "https://github.com/satnaing/astro-paper",
+        linkTitle: `${SITE.title} on LinkedIn`,
+        active: false,
+    },
+    {
+        name: "Mail",
+        href: "mailto:yourmail@gmail.com",
+        linkTitle: `Send an email to ${SITE.title}`,
+        active: false,
+    },
+    {
+        name: "Twitter",
+        href: "https://twitter.com/",
+        linkTitle: `${SITE.title} on Twitter`,
+        active: false,
+    },
+    {
+        name: "Twitch",
+        href: "https://github.com/satnaing/astro-paper",
+        linkTitle: `${SITE.title} on Twitch`,
+        active: false,
+    },
+    {
+        name: "YouTube",
+        href: "https://github.com/satnaing/astro-paper",
+        linkTitle: `${SITE.title} on YouTube`,
+        active: false,
+    },
+    {
+        name: "WhatsApp",
+        href: "https://github.com/satnaing/astro-paper",
+        linkTitle: `${SITE.title} on WhatsApp`,
+        active: false,
+    },
+    {
+        name: "Snapchat",
+        href: "https://github.com/satnaing/astro-paper",
+        linkTitle: `${SITE.title} on Snapchat`,
+        active: false,
+    },
+    {
+        name: "Pinterest",
+        href: "https://github.com/satnaing/astro-paper",
+        linkTitle: `${SITE.title} on Pinterest`,
+        active: false,
+    },
+    {
+        name: "TikTok",
+        href: "https://github.com/satnaing/astro-paper",
+        linkTitle: `${SITE.title} on TikTok`,
+        active: false,
+    },
+    {
+        name: "CodePen",
+        href: "https://github.com/satnaing/astro-paper",
+        linkTitle: `${SITE.title} on CodePen`,
+        active: false,
+    },
+    {
+        name: "Discord",
+        href: "https://github.com/satnaing/astro-paper",
+        linkTitle: `${SITE.title} on Discord`,
+        active: false,
+    },
+    {
+        name: "GitLab",
+        href: "https://github.com/satnaing/astro-paper",
+        linkTitle: `${SITE.title} on GitLab`,
+        active: false,
+    },
+    {
+        name: "Reddit",
+        href: "https://github.com/satnaing/astro-paper",
+        linkTitle: `${SITE.title} on Reddit`,
+        active: false,
+    },
+    {
+        name: "Skype",
+        href: "https://github.com/satnaing/astro-paper",
+        linkTitle: `${SITE.title} on Skype`,
+        active: false,
+    },
+    {
+        name: "Steam",
+        href: "https://github.com/satnaing/astro-paper",
+        linkTitle: `${SITE.title} on Steam`,
+        active: false,
+    },
+    {
+        name: "Telegram",
+        href: "https://github.com/satnaing/astro-paper",
+        linkTitle: `${SITE.title} on Telegram`,
+        active: false,
+    },
+    {
+        name: "Mastodon",
+        href: "https://github.com/satnaing/astro-paper",
+        linkTitle: `${SITE.title} on Mastodon`,
+        active: false,
+    },
+];
